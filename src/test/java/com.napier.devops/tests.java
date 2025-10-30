f747fccf4c778f90b26a696d1d5a0e26729ed89f
Miguel Gonzalez Sanchez <40800669@live.napier.ac.uk>
2025-10-30 13:01:44 +0000

    Adding auto .jar upload plus starting testing

---
+++ src/test/java/com.napier.devops/tests.java
@@ -0,0 +1,12 @@
+package com.napier.devops;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MyTest {
+
+    @Test
+    void sampleTest() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
