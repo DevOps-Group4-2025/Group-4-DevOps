7ea6ff0ca7296595e70f4fdf6ea7937f3eef8220
Miguel Gonzalez Sanchez <40800669@live.napier.ac.uk>
2025-11-08 19:12:11 +0000

    Testing

---
+++ src/test/java/com/napier/devops/repository/PopulationBreakdownRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.napier.devops.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PopulationBreakdownRepositoryTest {
+
+    @Test
+    void deleteInBatch() {
+    }
+
+    @Test
+    void getAllByContinent() {
+    }
+
+    @Test
+    void getAllByRegion() {
+    }
+
+    @Test
+    void getAllByCountry() {
+    }
+}
