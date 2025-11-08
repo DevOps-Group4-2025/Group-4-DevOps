c87b0dc0d750f7c6a2ece516807f871260168dda
Miguel Gonzalez Sanchez <40800669@live.napier.ac.uk>
2025-11-08 19:02:00 +0000

    new test class CapitalCityRepositoryTestIT and PopulationBreakdownTestIT

---
+++ src/test/java/com/napier/devops/model/PopulationBreakdownTestIT.java
@@ -0,0 +1,36 @@
+package com.napier.devops.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PopulationBreakdownTestIT {
+
+    @Test
+    void type() {
+    }
+
+    @Test
+    void name() {
+    }
+
+    @Test
+    void totalPopulation() {
+    }
+
+    @Test
+    void populationInCities() {
+    }
+
+    @Test
+    void populationNotInCities() {
+    }
+
+    @Test
+    void inCitiesPercentage() {
+    }
+
+    @Test
+    void notInCitiesPercentage() {
+    }
+}
